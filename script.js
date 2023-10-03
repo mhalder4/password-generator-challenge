e6ecf4cf5cdad03ccfc4f3f855bc0dd3a274729d
unknown <mhalder9555@gmail.com>
2023-10-02 22:50:42 -0500

    Added variables to keep track of and available characters

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length;
+var hasLowerCase;
+var hasUpperCase;
+var hasNumbers;
+var hasSpecialChar;
+var isValid;
+
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+var alphas = alphabet.split("");
+var numbers = number.split("");
+var specials = specialChar.split("");
+
+// function
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +29,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(alphas);
+console.log(numbers);
+console.log(specials);
